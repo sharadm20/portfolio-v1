9d334ef79546c0d950baf1c51140d197027b952c
sharad <sharadm2001@gmail.com>
2025-10-29 09:25:44 +0530

    Fix import path for About component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import About from './components/About';
+import About from './components/about';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Certifications from './components/Certifications';
